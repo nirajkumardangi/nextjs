fa0f3ecfe9f1ca379558aefd7755ec02c2d88072
nirajkumardangi <niraj8825224435@gmail.com>
2026-04-08 13:53:39 +0530

    Adding a Custom Componet to Layout & Styling Nest.js Project using CSS Modules

---
+++ 02-foodies-project/components/main-header.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import classes from "./main-header.module.css";
+
+import logoImage from "@/assets/logo.png";
+
+export default function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
+        <img src={logoImage.src} alt="A plate with food on it" />
+        NextLevel Food
+      </Link>
+
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
