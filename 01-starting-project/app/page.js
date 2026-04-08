51f320740612227d3f1f18be4320c7dc6a85eead
nirajkumardangi <niraj8825224435@gmail.com>
2026-04-08 12:19:51 +0530

    Navigating between pages

--- 01-starting-project/app/page.js
+++ 01-starting-project/app/page.js
@@ -1,11 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   console.log("Executing on server");
-  
+
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="/about">About</Link>
+      </p>
     </main>
   );
 }
