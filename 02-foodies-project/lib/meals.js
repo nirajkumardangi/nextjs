b6798dc2ed1294c57e36977f3d4c383002b3fd28
nirajkumardangi <niraj8825224435@gmail.com>
2026-04-09 12:12:44 +0530

    Loading & Rendering Meal Details via Dynamic Routes & Route Parameters

--- 02-foodies-project/lib/meals.js
+++ 02-foodies-project/lib/meals.js
@@ -3,7 +3,11 @@ import sql from "better-sqlite3";
 const db = sql("meals.db");
 
 export async function getMeals() {
-  await new Promise((resolve) => setTimeout(resolve, 3000)); // for extra 2 sec delay simulation
-//   throw new Error("failed while fetching!"); // for custom error simulation
+  await new Promise((resolve) => setTimeout(resolve, 2000)); // for extra 2 sec delay simulation
+  // throw new Error("failed while fetching!"); // for custom error simulation
   return db.prepare("SELECT * FROM meals").all();
+}
+
+export function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
