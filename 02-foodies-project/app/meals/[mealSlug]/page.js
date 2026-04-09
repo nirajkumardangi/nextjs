d4b2aea8ea438476438c3ce8605b34dff727d230
nirajkumardangi <niraj8825224435@gmail.com>
2026-04-09 13:27:55 +0530

    Throwing Not Found Errors For Individual Meals

--- 02-foodies-project/app/meals/[mealSlug]/page.js
+++ 02-foodies-project/app/meals/[mealSlug]/page.js
@@ -2,9 +2,14 @@ import Image from "next/image";
 import classes from "./page.module.css";
 
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealsDetailesPage({ params }) {
   const meal = getMeal(params.mealSlug);
+
+  if(!meal) {
+    notFound();
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
