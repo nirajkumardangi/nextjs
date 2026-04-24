54b4546cbf908097df28e98bc0e3a483adc9f10c
nirajkumardangi <niraj8825224435@gmail.com>
2026-04-24 07:56:56 +0530

    feat: enhance post functionality with optimistic updates and revalidation

--- 06-mutating-data-deep-dive/actions/posts.js
+++ 06-mutating-data-deep-dive/actions/posts.js
@@ -3,6 +3,7 @@
 import { redirect } from "next/navigation";
 import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudinary";
+import { revalidatePath } from "next/cache";
 
 // form action
 export async function createPost(prevState, formData) {
@@ -38,17 +39,19 @@ export async function createPost(prevState, formData) {
     );
   }
 
-  storePost({
+  await storePost({
     imageUrl,
     title,
     content,
     userId: 1,
   });
 
+  revalidatePath("/", "layout");
   redirect("/feed");
 }
 
 // form action for toggle post like
 export async function togglePostLikeStatus(postId) {
-  updatePostLikeStatus(postId, 2);
+  await updatePostLikeStatus(postId, 2);
+  revalidatePath("/", "layout");
 }
