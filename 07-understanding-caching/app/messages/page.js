1a4ecfcd2d05a4d0d23ea2d5ad8b3bd940d32926
nirajkumardangi <niraj8825224435@gmail.com>
2026-04-24 16:25:18 +0530

    Controlling Data Caching

--- 07-understanding-caching/app/messages/page.js
+++ 07-understanding-caching/app/messages/page.js
@@ -1,8 +1,23 @@
 import Messages from "@/components/messages";
+import { unstable_noStore } from "next/cache";
+
+//== pre-defined variable for revalidation after 5 sec
+// export const revalidate = 5
+
+//== pre-defined variable for avoid cache
+// export const dynamic = "force-dynamic";
+
 
 export default async function MessagesPage() {
+  // pre-defined next/cache function that avoid caching
+  unstable_noStore()
   const response = await fetch("http://localhost:8080/messages", {
-    cache: "no-cache",
+    //== avoid caching
+    // cache: "no-cache",
+    //== after 5 sec the fetch request revalidate.
+    // next: {
+    //   revalidate: 5,
+    // },
   });
   const messages = await response.json();
 
